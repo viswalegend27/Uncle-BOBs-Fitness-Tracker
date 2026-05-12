f887a3873995d11adb47c6a84f5ba437567b0af9
Viswa v <viswamalar2004@gmail.com>
2026-05-12 20:03:09 +0530

    Created UI + Python form created For create-blog

--- main/my_trainer/trainer_bob/forms.py
+++ main/my_trainer/trainer_bob/forms.py
@@ -94,3 +94,11 @@ class DietPlannerForm(forms.Form):
                 raise forms.ValidationError("Invalid body measurements")
 
         return cleaned_data
+
+
+class BlogForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(widget=forms.Textarea)
+    image = forms.URLField(required=True)
+
+
--- main/my_trainer/trainer_bob/static/trainer_bob/scripts/dietdetail.js
+++ main/my_trainer/trainer_bob/static/trainer_bob/scripts/dietdetail.js
@@ -1,13 +1,14 @@
 // explicit declaration of my modal window.
 const modal = document.querySelector(".float-window");
 const content = document.querySelector(".main-content");
-
+const createBlogBtn = document.querySelector(".create-blog");
+const createBlog = document.querySelector(".blog-window");
 
 // logic for modal appearence when clicking read-more.
 document.querySelectorAll(".read-more").forEach(link => { 
   link.addEventListener("click", function(e) {
-  e.preventDefault();
-  const title = this.closest(".text-cont").querySelector("h3").innerText;
+  e.preventDefault(); // preventing default reaload anim.
+  const title = this.closest(".text-cont").querySelector("h3").innerText; // getting the actual title from the code.
   let contentData = [];
 
   if (title.toLowerCase().includes("deficit")) {
@@ -19,13 +20,32 @@ document.querySelectorAll(".read-more").forEach(link => {
   const modalContent = document.getElementById("modal-content");
   modalContent.innerHTML = "";
 
-  contentData.forEach(item => {
-    modalContent.innerHTML += `<p>• ${item}</p>`;
-  });
+  modalContent.innerHTML = `<p>${contentData}</p>`;
   document.getElementById("output").innerText = title;
-  content.classList.add("show");
   modal.classList.add("show");
   });
+});
+
+// logic for create-blog form
+
+createBlogBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  createBlog.classList.add("show");
+});
+
+// close logic 1 for create-blog form
+createBlog.addEventListener("click", (event)=> {
+    if (event.target.classList.contains("blog-window")) {
+      createBlog.classList.remove("show");
+    }
+});
+
+// logic for closing create-blog window.
+document.querySelector(".blog-close-btn").addEventListener("click", () => {
+  // removing my show from previous read-more tab.
+  createBlog.classList.remove("show");
+  // removing hide from content and enabling content to show.
+  content.classList.remove("hide");
 });
 
 // logic for close-btn inside modal to reappear the actual window.
@@ -41,6 +61,5 @@ document.querySelector(".float-window").addEventListener("click", (e) => {
   if (e.target.classList.contains("float-window")) {
     modal.classList.remove("show");
     content.classList.remove("hide");
-    content.classList.add("show");
   }
 });
