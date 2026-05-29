727a6e8145d4c1c8db132afbd0e1554f936f6078
Viswa v <viswamalar2004@gmail.com>
2026-05-29 20:47:59 +0530

    Sign-in Hide/Show Logic complete ✅👏

--- main/my_trainer/trainer_bob/static/trainer_bob/scripts/signIn.js
+++ main/my_trainer/trainer_bob/static/trainer_bob/scripts/signIn.js
@@ -14,3 +14,24 @@ document.getElementById('togglePw').addEventListener('click', () => {
   }
   lucide.createIcons();
 });
+
+// Create account
+
+const goToSignup = document.getElementById("createAccount");
+const signIn = document.getElementById("signIn");
+const signUp = document.getElementById("signUp");
+const backToLogin = document.getElementById("backToLogin");
+
+goToSignup.addEventListener("click", (e) => {
+  if (signIn.classList.contains('show')) {
+    signUp.classList.replace("hidden", "show");
+    signIn.classList.replace("show", "hidden");
+  }
+});
+
+backToLogin.addEventListener("click", (e) => {
+  if (signUp.classList.contains('show')) {
+    signIn.classList.replace("hidden", "show");
+    signUp.classList.replace("show", "hidden");
+  }
+});
